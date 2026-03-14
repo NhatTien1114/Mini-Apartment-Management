a03b9f5033fe82af54a6b5aea5074ba81a23fc8b
unknown <tongnguyennhattien140805@gmail.com>
2026-03-14 22:14:57 +0700

    ui: update QuanLy and KhachThue UI

--- src/UI/main/QuanLyUI.java
+++ src/UI/main/QuanLyUI.java
@@ -3,6 +3,7 @@ package ui.main;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import ui.util.RoundedButton;
 
 public class QuanLyUI {
     private final Color MAU_NEN = new Color(229, 231, 235);
@@ -13,19 +14,42 @@ public class QuanLyUI {
         pnl.setBorder(new EmptyBorder(20, 20, 20, 20));
         pnl.setBackground(MAU_NEN);
         
+        JPanel pnlTop = new JPanel();
+        pnlTop.setLayout(new BorderLayout());
         JLabel lblTitle = new JLabel("Quản Lý Chung Cư");
+        RoundedButton btnThemPhong = new RoundedButton("+ Thêm phòng",17);
         lblTitle.setFont(new Font("Be Vietnam Pro", Font.BOLD, 24));
-        pnl.add(lblTitle, BorderLayout.NORTH);
+        pnlTop.add(lblTitle, BorderLayout.WEST);
+        pnlTop.add(btnThemPhong, BorderLayout.EAST);
+        btnThemPhong.setFont(new Font("Be Vietnam Pro", Font.BOLD, 17));
+        btnThemPhong.setBackground(new Color(65, 113, 201));
+        btnThemPhong.setForeground(Color.WHITE);
+        pnl.add(pnlTop, BorderLayout.NORTH);
+        pnlTop.setBackground(MAU_NEN);
         
         JPanel pnlContent = new JPanel();
+        JPanel pnlContentTop = new JPanel();
+        pnlContentTop.setLayout(new BorderLayout());
         pnlContent.setBackground(Color.WHITE);
         pnlContent.setLayout(new BorderLayout());
-        JLabel lblContent = new JLabel("Nội dung quản lý sẽ được cập nhật");
-        lblContent.setFont(new Font("Be Vietnam Pro", Font.PLAIN, 14));
-        lblContent.setHorizontalAlignment(SwingConstants.CENTER);
-        pnlContent.add(lblContent, BorderLayout.CENTER);
+        JTextField txtTimPhong = new JTextField(20);
+        txtTimPhong.setPreferredSize(new Dimension(200, 30));
+        pnlContentTop.add(txtTimPhong, BorderLayout.WEST);
+        pnlContentTop.setBackground(MAU_NEN);
+        pnlContent.add(pnlContentTop, BorderLayout.NORTH);
         
-        pnl.add(pnlContent, BorderLayout.CENTER);
+        String[] columnNames = {"Phòng", "Tầng", "Phòng đơn", "Diện tích", "Giá thuê", "Trạng thái"};
+        Object[][] data = {
+            {"T1.01", "Tầng 1", "Phòng đơn", "20m2", 500000, "Đã Cọc"},
+            {"T1.02", "Tầng 1", "Phòng đơn", "20m2", 500000, "Đã Cọc"},
+            {"T1.03", "Tầng 1", "Phòng đơn", "20m2", 500000, "Đã Cọc"}
+        };
+        JTable table = new JTable(data, columnNames);
+        JScrollPane scrollPane = new JScrollPane(table);
+        table.getTableHeader().setFont(new Font("Be Vietnam Pro", Font.BOLD, 14));
+        
+        
+        pnl.add(scrollPane, BorderLayout.CENTER);
         return pnl;
     }
 }
